f8f0163ed3e72390b71dc5342d97b268fb89084f
Samina-tenso <tensammi@gmail.com>
2022-03-14 20:35:08 +0100

    doneish

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <script setup>
+
 import {ref} from "vue"
+ 
 const newTodo = ref ("")
+const todos = ref([])
+   function toggleDone(todo){
+            todo.completed = !todo.completed
+        }
+
+        function removeTodo(index){
+            todos.value.splice(index,1)
+        }
+        function markAllDone(){
+            todos.value.forEach((todo)=> todo.completed =true)
+        }
 function addNewTodo(){
-    console.log("form was submitted")
+    todos.value.push({
+        id: Date.now(),
+        completed: false, 
+        content: newTodo.value,
+        });
+
+        newTodo.value="";
+
+
+      
+         
     return {
     addNewTodo,
     newTodo,
+    todos,
+    toggleDone,
+    markAllDone
 }
 }
+ 
+
+
 
 </script>
 
@@ -15,12 +44,26 @@ function addNewTodo(){
 <h1>Vue your todo</h1>
 <form @submit.prevent="addNewTodo"> 
     <label> New Todo</label>
-    <input v-model="newTodo" name="new-todo"> 
+    <input v-model="newTodo" name="newTodo"> 
     <button>Add </button>
 </form>
+<button @click="markAllDone"> Mark All Done</button>
+<ul>
+
+<li v-for="(todo,index) in todos" :key="todo.id" class="todo">
+    <h3 :class="{completed: todo.completed}" @click="toggleDone(todo)"> {{todo.content}}</h3>
+    <button @click="removeTodo(index)"> Remove Todo</button>
+     </li>
+     </ul>
 
 </template>
 
 <style>
-
+.todo{
+    cursor: pointer;
+}
+.completed {
+    text-decoration:line-through; 
+    
+}
 </style>
